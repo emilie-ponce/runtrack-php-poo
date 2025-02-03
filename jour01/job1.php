aafdc6c4364ec709eeefadf68b33b2905f9385e0
emilie-ponce <emilie.ponce@laplateforme.io>
2025-02-03 15:10:46 +0100

    j'ai modifié le job 1 en déplaçant les echo à l'intérieur de la class

--- jour01/job1.php
+++ jour01/job1.php
@@ -2,16 +2,18 @@
     class Operation {
         public $nombre1;
         public $nombre2;
-    
-        public function __construct($nombre1 = 17, $nombre2 = 11){
+
+        public function __construct($nombre1 = 17, $nombre2 = 11) {
             $this->nombre1 = $nombre1;
             $this->nombre2 = $nombre2;
+        }
+
+        public function afficherAttributs() {
+            echo "Nombre1: " . $this->nombre1 . "<br>";
+            echo "Nombre2: " . $this->nombre2 . "<br>";
         }
     }
 
     $operation = new Operation();
-
-    echo "Nombre1: " . $operation->nombre1 . "\n";
-    echo "Nombre2: " . $operation->nombre2 . "\n";
-
+    $operation->afficherAttributs();
 ?>
