c44300393d1d808b41f5bddc30736db292f8843a
emilie-ponce <emilie.ponce@laplateforme.io>
2025-02-03 15:22:01 +0100

    pour le job2 j'ai récupéré le code du job 1 pour rajouter la public function addition pour donc additionner les deux variables, et pour que ça fonctionne j'ai mit en dehors de la class une instance addition

--- jour01/job2.php
+++ jour01/job2.php
@@ -1,3 +1,25 @@
 <?php
-    
+    class Operation {
+        public $nombre1;
+        public $nombre2;
+
+        public function __construct($nombre1 = 17, $nombre2 = 11) {
+            $this->nombre1 = $nombre1;
+            $this->nombre2 = $nombre2;
+        }
+
+        public function afficherAttributs() {
+            echo "Nombre1: " . $this->nombre1 . "<br>";
+            echo "Nombre2: " . $this->nombre2 . "<br>";
+        }
+        
+        public function addition() {
+            $resultat = $this->nombre1 + $this->nombre2;
+            echo "Le résultat de l'addition est : " . $resultat . "<br>";
+        }
+    }
+
+    $operation = new Operation();
+    $operation->afficherAttributs();
+    $operation->addition();
 ?>
